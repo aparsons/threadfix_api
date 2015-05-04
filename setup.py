6ab84e9d382d86bc5bb0769f931783c6d022a403
Adam Parsons <adam@aparsons.net>
2015-05-04 07:34:05 -0500

    Marking as beta

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=['requests'],
     keywords=['threadfix', 'api', 'security', 'software'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
