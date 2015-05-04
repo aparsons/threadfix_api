df3bd01905e61a23b508be42a64fb8ecb18528f3
Adam Parsons <adam@aparsons.net>
2015-05-04 01:18:19 -0500

    Updated for creating local builds

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
+from threadfix_api import __version__ as version
 
 
 setup(
     name='threadfix_api',
     packages=['threadfix_api'],
-    version='1.0.0',
+    version=version,
     description='An API wrapper to facilitate interactions to and from ThreadFix.',
     author='Adam Parsons',
     author_email='adam@aparsons.net',
@@ -13,4 +15,14 @@ setup(
     license='MIT',
     install_requires=['requests'],
     keywords=['threadfix', 'api', 'http'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
 )
